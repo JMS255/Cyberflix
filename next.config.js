d838ad9d35a844dc54ff2bf62c90caee4883674d
James <jamesignacio255@gmail.com>
2026-06-17 01:06:53 +0800

    fix: add CSP headers to allow VidSrc and embed iframe sources

--- next.config.js
+++ next.config.js
@@ -9,6 +9,39 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data: https://image.tmdb.org https:",
+              "media-src 'self' https:",
+              "connect-src 'self' https://api.themoviedb.org",
+              [
+                "frame-src 'self'",
+                "https://vidsrc.to",
+                "https://vidsrc.me",
+                "https://www.2embed.cc",
+                "https://player.smashy.stream",
+                "https://embed.smashystream.com",
+              ].join(' '),
+              "frame-ancestors 'none'",
+            ].join('; '),
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
